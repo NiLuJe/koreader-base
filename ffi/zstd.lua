a89d6ab48a7ca2c318a12ac2f3c4363d349313c2
NiLuJe <ninuje@gmail.com>
2020-12-07 22:42:56 +0100

    zstd: 	* Switch to explicit memory allocations 	* Instead of taking/returning a Lua string copy, 	  take/return a cdata ptr, size tuple. 	  (Technically, void *, size_t)

--- ffi/zstd.lua
+++ ffi/zstd.lua
@@ -5,6 +5,9 @@ LuaJIT FFI wrapper for ZSTD.
 ]]
 
 local ffi = require("ffi")
+local C = ffi.C
+
+local posix = require("ffi/posix_h") -- luacheck: ignore 211
 local _ = require("ffi/zstd_h")
 
 local zst
@@ -19,22 +22,24 @@ end
 local zstd = {}
 
 -- c.f., https://github.com/facebook/zstd/tree/dev/examples
-function zstd.zstd_compress(data)
-    local n = zst.ZSTD_compressBound(#data)
-    local cbuff = ffi.new("uint8_t[?]", n)
+function zstd.zstd_compress(ptr, size)
+    local n = zst.ZSTD_compressBound(size)
+    local cbuff = C.calloc(n, 1)
+    assert(cbuff ~= nil)
     -- NOTE: We should be quite all right with the default (3), which will most likely trounce zlib's 9 in every respect...
-    local clen = zst.ZSTD_compress(cbuff, n, data, #data, zst.ZSTD_CLEVEL_DEFAULT)
+    local clen = zst.ZSTD_compress(cbuff, n, ptr, size, zst.ZSTD_CLEVEL_DEFAULT)
     assert(zst.ZSTD_isError(clen) == 0)
-    return ffi.string(cbuff, clen)
+    return cbuff, clen
 end
 
-function zstd.zstd_uncompress(zstdata)
+function zstd.zstd_uncompress(ptr, size)
     -- The decompressed size is encoded in the ZST frame header
-    local n = zst.ZSTD_getFrameContentSize(zstdata, #zstdata)
-    local buff = ffi.new("uint8_t[?]", n)
-    local ulen = zst.ZSTD_decompress(buff, n, zstdata, #zstdata)
+    local n = zst.ZSTD_getFrameContentSize(ptr, size)
+    local buff = C.calloc(n, 1)
+    assert(buff ~= nil)
+    local ulen = zst.ZSTD_decompress(buff, n, ptr, size)
     assert(zst.ZSTD_isError(ulen) == 0)
-    return ffi.string(buff, ulen)
+    return buff, ulen
 end
 
 return zstd
